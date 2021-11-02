3bdee21899ef8a6f82a31ebc1a1fa0624bee5b1e
Accountant-riyad <Accountant-riyad>
2021-11-02 07:34:56 +0600

    first commit

---
+++ assets/schemas.js
@@ -0,0 +1,26 @@
+
+      var starting_value = [
+        {
+          name: "John Smith",
+          age: 35,
+          gender: "male",
+          location: {
+            city: "San Francisco",
+            state: "California",
+            citystate: ""
+          },
+          pets: [
+            {
+              name: "Spot",
+              type: "dog",
+              fixed: true
+            },
+            {
+              name: "Whiskers",
+              type: "cat",
+              fixed: false
+            }
+          ]
+        }
+      ];
+      
